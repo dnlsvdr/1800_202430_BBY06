6a8c449a8354eb8bc4d28dbb58a1109a701d6c3b
dnlsvdr <danielorcasaavedra778@gmail.com>
2025-03-18 14:52:30 -0700

    fix the setting.html

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,4 +1,15 @@
-const home = document.getElementById('home');
-home.addEventListener('click', () => {
-    window.location.href = 'home.html';
+document.addEventListener('DOMContentLoaded', () => {
+    const home = document.getElementById('home');
+    if (home) {
+        home.addEventListener('click', () => {
+            window.location.href = 'home.html';
+        });
+    }
+
+    const settings = document.getElementById('settings');
+    if (settings) {
+        settings.addEventListener('click', () => {
+            window.location.href = 'settings.html';
+        });
+    }
 });
